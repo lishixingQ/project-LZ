a4d18ee6795cba618113107f5d5447d212b4d772
lishixing <1245064121@qq.com>
2023-05-30 16:10:59 +0800

    lsx第1次提交master分支，项目创建

---
+++ js/rem.js
@@ -0,0 +1,20 @@
+function setRem() {
+    var ui_w = 375;
+
+    // 获取屏幕的宽度
+
+    var client_w = document.documentElement.clientWidth || document.body.clientWidth;
+
+    // 通过js动态改变html根节点字体大小
+
+    var html_ = document.getElementsByTagName('html')[0];
+
+    html_.style.fontSize = (client_w / ui_w) * 10 + 'px';
+
+}
+
+// window.onresize 浏览器被重置大小执行事件
+
+window.onresize = setRem;
+
+window.onload = setRem;
